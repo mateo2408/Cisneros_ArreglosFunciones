0f8476b5ef1e0d6be9e1c5732733e49c991176fb
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-11-30 21:46:37 -0500

    Definimos las matrices, sus filas y columnas

--- suma_matrices.c
+++ suma_matrices.c
@@ -1,3 +1,6 @@
+//Mateo Cinseros
+//Actualize el ejercicio de suma de matrices, utilizando el concepto de Funciones.
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
@@ -6,4 +9,28 @@
 int suma(int a, int b)
 {
     return a+b;
+}
+
+int main()
+{
+    //Definimos el numero de filas y columnas
+    int filas;
+    int columnas;
+    printf("Ingrese la cantidad de filas de las matrices: ");
+    scanf("%d", &filas);
+    printf("Ingrese la cantidad de columnas de las matrices: ");
+    scanf("%d", &columnas);
+
+    //Definir matrices
+    int A[filas][columnas];
+    int B[filas][columnas];
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            A[i][j]=rand()%100;
+            B[i][j]=rand()%100;
+        }
+    }
+    return 0;
 }
